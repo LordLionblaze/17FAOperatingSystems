4b08afd4291b07b2402f2ae35308b95a61226436
LordLionblaze <hdcuey331@iwcc.edu>
2017-11-09 14:37:06 -0600

    Intro to bash scripting and variables.

---
+++ name.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+read -p "Hello! What is your name?" name
+read -p "What is your age?" age
+read -p "What is your favorite color?" color
+echo "So your name is $name, you are $age years old and your favorite color is
+$color."
+
